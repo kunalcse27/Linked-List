204b17c863b37a3c9e29795e1ccdd93a63354c7c
kunalcse27 <kunal.cse27@gmail.com>
2026-03-18 01:25:00 +0530

    Add nthNode method to find node from end
    Implement a method to find the nth node from the end of a linked list. Includes a main method for testing.

---
+++ nthNodefromEnd.java
@@ -0,0 +1,49 @@
+public class nthNodefromEnd {
+    
+    public static Node nthNode(Node head, int n) {
+        int size=0;
+        Node temp=head;
+        // Count size
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+
+        int a = size - n + 1;
+
+        // Reset temp to head
+        temp = head;
+        for (int i=1; i<=a - 1;i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(12);
+        Node c = new Node(13);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(100);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        Node result = nthNode(a, 2); // Example: 2nd node from end
+        System.out.println(result.data); // Output: 50
+    }
+}
